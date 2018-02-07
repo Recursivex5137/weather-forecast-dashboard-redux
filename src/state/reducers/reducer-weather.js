1e12faed454929b6ca12d6058984a7317b2c1b5d
Recursivex5137 <recursivex5137@gmail.com>
2018-02-07 10:17:12 -0500

    Update reducer-weather to properly fetch the data from the action.

--- src/state/reducers/reducer-weather.js
+++ src/state/reducers/reducer-weather.js
@@ -1,5 +1,10 @@
-export default (state=null, action) =>  {
-  console.log('Action Recieved : ');
-  console.log(action);
+import { FETCH_WEATHER } from '../actions/index';
+
+export default (state=[], action) =>  {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      const data = [action.payload.data, ...state];
+      return data;
+  }
   return state;
 };
