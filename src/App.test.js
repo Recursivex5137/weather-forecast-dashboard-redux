8ad0229140390bdf00200b4ad912d719d3ac8069
Recursivex5137 <recursivex5137@gmail.com>
2018-02-07 09:27:44 -0500

    Updated App.test to pass by reflecting the latest changes by hooking up redux and middleware.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
+
 import App from './App';
+import reducers from './state/reducers';
+
 
 it('renders without crashing', () => {
+  const createStoreWithMiddleWare = applyMiddleware(ReduxPromise)(createStore);
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+  <Provider store={createStoreWithMiddleWare(reducers)}>
+    <App />
+  </Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
